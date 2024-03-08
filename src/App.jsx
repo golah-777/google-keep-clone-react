1f0a21cc5dc2bd090fd31de7d7946ea95c7c808d
golah-777 <128902542+golah-777@users.noreply.github.com>
2024-03-08 17:00:31 +0200

    heeloo

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,64 @@
-
-import Sidebar from './components/Sidebar/Sidebar'
-import Nevbar from './components/Nevbar/Nevbar'
-import Form from './components/Form/Form'
-import Notes from './components/Notes/Notes'
-import Modal from './components/Modals/Modal'
-import React, { useState } from "react"
-import { v4 as uuidv4 } from 'uuid';
+import Sidebar from "./components/Sidebar/Sidebar";
+import Nevbar from "./components/Nevbar/Nevbar";
+import Form from "./components/Form/Form";
+import Notes from "./components/Notes/Notes";
+import Modal from "./components/Modals/Modal";
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 uuidv4();
-import './index.css'
+import "./index.css";
 
 function App() {
   const [notes, setNotes] = useState([]);
-  const addNotes = (title, text)=>{ 
-    const newNotes = [...notes,{id:uuidv4() , title: title , text: text }]
-    setNotes(()=> newNotes);
+  const [selectedNote, setselectedNote] = useState({});
+  const [isModalOpen, setModalOpen] = useState(false);
+  const addNotes = (title, text) => {
+    const newNotes = [...notes, { id: uuidv4(), title: title, text: text }];
+    setNotes(() => newNotes);
+  };
+
+  const deleteNote = (id) => {
+    setModalOpen(()=>false); 
+    setNotes(notes.filter((note) => id != note.id));
+  };
+
+  const noteInfo = (id)=>{
+    {notes.map(note=> {
+      if(id === note.id){
+        setselectedNote(()=> note);
+      }
+    })}
   }
 
-  console.log(notes)
-
-  const deleteNote = (id)=>{
-    setNotes(notes.filter(note=> id != note.id ))
-  }
+  const openModal = () => {
+    setModalOpen(()=>true);
+  };
+  const modalClose=()=> {
+    setModalOpen(()=>false);
+  };
 
   return (
     <div className="App">
       <Nevbar></Nevbar>
       <Sidebar></Sidebar>
-      <Form addNotes={addNotes}></Form> 
-      <Notes notes = {notes}  deleteNote={deleteNote}></Notes>
-      <Modal></Modal>
+      <Form 
+       addNotes={addNotes}
+       selectedNote = {selectedNote}
+      ></Form>
+      <Notes
+        notes={notes}
+        deleteNote={deleteNote}
+        openModal={openModal}
+        noteInfo = {noteInfo}
+      ></Notes>
+      <Modal 
+       isModalOpen={isModalOpen}
+       modalClose = { modalClose }
+       openModal={openModal}
+       selectedNote = {selectedNote}
+      ></Modal>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Notes/Notes.jsx
+++ src/components/Notes/Notes.jsx
@@ -1,16 +1,21 @@
+import Note from "./Note";
 
-import Note from './Note'
-
-export default function Notes({ notes, deleteNote }){
-
-  return(
+export default function Notes({ notes, deleteNote, openModal, noteInfo }) {
+  return (
     <div className="notes">
       {notes.length === 0 && <p>Add a note</p>}
-      { notes.length !== 0 && notes.map((note)=>
-        <Note key={note.id} id={note.id} title={note.title} text={note.text} deleteNote = {deleteNote}></Note>
-       )
-      }
-         
-   </div>
-  )
+      {notes.length !== 0 &&
+        notes.map((note) => (
+          <Note
+            key={note.id}
+            id={note.id}
+            title={note.title}
+            text={note.text}
+            deleteNote={deleteNote}
+            openModal={openModal}
+            noteInfo = {noteInfo}
+          ></Note>
+        ))}
+    </div>
+  );
 }
